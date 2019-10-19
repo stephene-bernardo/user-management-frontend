c35b2b46708bad59aa2919c4391d50d3f71a47b5
Karl Bernardo <stephene.bernardo@gmail.com>
2019-10-19 20:23:49 +0800

    added GUI for usermanagement

---
+++ src/user.js
@@ -0,0 +1,45 @@
+import React from "react";
+import  { useRef, useState  } from 'react'
+
+export default function User (props) {
+  const [isEditing, setIsEditing] = useState(false);
+  const firstName = useRef(  null);
+  const lastName = useRef(null);
+  const userName = useRef(null);
+  function handleEdit () {
+    setIsEditing(true)
+  }
+  function handleSave() {
+    props.handleEditButton({variables: {
+        id: props.value.id,
+        firstName: firstName.current.value,
+        lastName: lastName.current.value,
+        userName: userName.current.value
+      }});
+    setIsEditing(false)
+  }
+  let toggle;
+  if(!isEditing){
+    toggle = (<React.Fragment>
+      <td>{props.value.userName}</td>
+      <td>{props.value.firstName}</td>
+      <td>{props.value.lastName}</td>
+      <td><button onClick={handleEdit}>edit</button></td>
+    </React.Fragment>)
+  } else {
+    toggle = (
+      <React.Fragment>
+        <td><input type="text" ref={userName}   defaultValue={props.value.userName} placeholder="User Name"/></td>
+        <td><input type="text"  ref={firstName} defaultValue={props.value.firstName}  placeholder="First Name"/></td>
+        <td><input type="text" ref={lastName} defaultValue={props.value.lastName} placeholder="Last Name"/></td>
+        <td><button onClick={handleSave}>save</button></td>
+      </React.Fragment>
+    )
+  }
+  return (
+    <tr>
+      {toggle}
+      <td><button onClick={()=>props.handleDeleteButton({variables: {id: props.value.id}})}>remove</button></td>
+    </tr>
+  )
+}
--- src/userManagement.js
+++ src/userManagement.js
@@ -1,34 +1,65 @@
 import React from 'react';
-import { ApolloClient } from 'apollo-client';
-import gql from "graphql-tag";
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import {useQuery, useMutation} from "@apollo/react-hooks";
+import gql from 'graphql-tag';
+import  { useRef } from 'react'
+import User from "./user";
+const FETCH_USERS = gql`{users {id, firstName, lastName, userName}}`;
+const CREATE_USER = gql`mutation CreateUser($firstName: String!, $lastName: String!, $userName: String!)
+     {createUser(user: {firstName:$firstName, lastName:$lastName, userName: $userName}){id}}`;
+const DELETE_USER = gql `mutation DeleteUser($id:Int!){deleteUser(id: $id)}`;
+const UPDATE_USER = gql `mutation UpdateUser($id: Int!, $firstName: String!, $lastName: String!, $userName: String!)
+{updateUser(id: $id, userInput: {firstName: $firstName, lastName: $lastName, userName: $userName})}`
 
-export default class UserManagement extends React.Component {
-  constructor(props){
-    super(props);
-    const link = new HttpLink({
-      uri: 'http://localhost:4201/graphql'
-    });
-
-    const cache = new InMemoryCache();
-    console.log('elo')
-    const client = new ApolloClient({
-      cache,
-      link,
-      fetchOptions: {
-        mode: 'no-cors',
-      },
-    });
-    client.query({
-        query: gql`{users {id, firstName, lastName, userName}}`
-      })
-      .then(result => console.log(result));
-
+export default function UserManagement () {
+  const firstName = useRef(null);
+  const lastName = useRef(null);
+  const userName = useRef(null);
+  const { data } = useQuery(FETCH_USERS);
+  const [CreateUser]= useMutation(CREATE_USER, {refetchQueries: mutationResult => [{query: FETCH_USERS}]})
+  const [DeleteUser]= useMutation(DELETE_USER, {refetchQueries: mutationResult => [{query: FETCH_USERS}]})
+  const [UpdateUser]= useMutation(UPDATE_USER, {refetchQueries: mutationResult => [{query: FETCH_USERS}]})
+  function handleClear(){
+    firstName.current.value = '';
+    lastName.current.value = '';
+    userName.current.value = '';
   }
-
-  render(){
-    return <div>User Management1</div>
+  function handleClick(){
+    CreateUser({variables: {firstName: firstName.current.value, lastName: lastName.current.value,
+        userName: userName.current.value
+    }})
   }
-
+  let tableEntry = [];
+  if(data && data.users){
+    tableEntry =  data.users
+      .sort(({ id: previousID }, { id: currentID }) => previousID - currentID)
+      .map(value=> <User key={value.id}
+                         handleEditButton={UpdateUser}
+                         handleDeleteButton={DeleteUser}
+                         value={value}/>)
+  }
+  return (
+    <table>
+      <thead>
+      <tr>
+        <th>User Name</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th></th>
+        <th></th>
+      </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><input type="text" ref={userName} placeholder="User Name"/></td>
+          <td><input type="text" ref={firstName} placeholder="First Name"/></td>
+          <td><input type="text" ref={lastName} placeholder="Last Name"/></td>
+          <td><button onClick={handleClick}>add</button></td>
+          <td><button onClick={handleClear}>clear</button></td>
+        </tr>
+        <tr></tr>
+        <tr></tr>
+        {tableEntry}
+      </tbody>
+    </table>
+  )
 }
